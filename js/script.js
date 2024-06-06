af7097bcdf6e6057492bbd2de715a07950b3a1f3
fauzangit <ali.fauzan786@gmail.com>
2024-06-06 22:25:56 +0700

    ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Ini Javascript
+//Javascript
 
 function formValidation() {
     let nameInput = document.getElementById('name-input').value;
@@ -31,3 +31,28 @@ function showBanner(indexBanner) {
 }
 
 setInterval(() => nextSlide(1), 3000);
+
+//
+function validateForm(){
+    const nama = document.forms["contus"]["nama"].value;
+    const nohp = document.forms["contus"]["nohp"].value;
+    const email = document.forms["contus"]["email"].value;
+    const pilihKursus = document.forms["contus"]["pilihkursus"].value;
+    const pesan = document.forms["contus"]["pesan"].value;
+
+    if (nama != "" && nohp != "" && email != "" && pilihKursus != "" && pesan != ""){
+        document.getElementById("sukses-submit").innerHTML = "Data Tersimpan"
+    }
+
+    setSenderUI(nama, nohp, email, pilihKursus, pesan);
+
+    return false;
+}
+
+function setSenderUI(nama, nohp, email, pilihKursus, pesan){
+    document.getElementById("sender-nama").innerHTML = nama;
+    document.getElementById("sender-nohp").innerHTML = nohp;
+    document.getElementById("sender-email").innerHTML = email;
+    document.getElementById("sender-pilihkursus").innerHTML = pilihKursus;
+    document.getElementById("sender-pesan").innerHTML = pesan;
+}
